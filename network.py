75f8943e4325f2c2d55a8cae885fa978fac72787
SergeyParamonov <sergey.paramonov@phystech.edu>
2014-06-27 20:35:35 +0200

    added pulse database to monitor_loop

--- network.py
+++ network.py
@@ -255,8 +255,8 @@ def create_dict_id_title(dict_id_title,id_title_database):
 
 #REQUIRES LIBRARIES datetime (datetime constructor), pytz, 
 #DEPENDS ON SRC/ *monitor_visualize.py*: create_monitor_figure, *utils.py*: make_fig_key, clean_old, update_posts, *network.py* update_date_dictionary, create_dict_id_title, extract_post_all_info
-#MODIFIES dict_dates, dict_last_values, dict_id_title,  id_title_database, PoolManager (close connections), cache, monitor_database
-def monitor_call(dict_dates, dict_id_title, monitor_database, id_title_database, cache, monitor_datatypes):
+#MODIFIES dict_dates, dict_last_values, dict_id_title,  id_title_database, PoolManager (close connections), cache, monitor_database, pulse_database
+def monitor_call(dict_dates, dict_id_title, monitor_database, id_title_database, cache, monitor_datatypes, pulse_database):
   # remove the posts older than 2 days
   clean_old(dict_dates, monitor_database, id_title_database, monitor_datatypes, cache)
   # remove all old records from monitor database, this might happen due to errors or restarts of the server
